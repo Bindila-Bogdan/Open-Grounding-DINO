1892493ac16c98da2ca9f6c62d0e14f232bf19bc
Bindila-Bogdan <bogdan@meshh.com>
2025-06-16 00:15:31 +0200

    update scripts for training and testing

--- train_dist.sh
+++ train_dist.sh
@@ -7,13 +7,9 @@ NODE_RANK=${NODE_RANK:-0}
 PORT=${PORT:-29500}
 MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 
-# Change ``pretrain_model_path`` to use a different pretrain. 
-# (e.g. GroundingDINO pretrain, DINO pretrain, Swin Transformer pretrain.)
-# If you don't want to use any pretrained model, just ignore this parameter.
-
 python -m torch.distributed.launch  --nproc_per_node=${GPU_NUM} main.py \
         --output_dir ${OUTPUT_DIR} \
         -c ${CFG} \
         --datasets ${DATASETS}  \
-        --pretrain_model_path ./weights/groundingdino_swint_ogc.pth \
+        --pretrain_model_path "./weights/groundingdino_swint.pth" \
         --options text_encoder_type="bert-base-uncased"
