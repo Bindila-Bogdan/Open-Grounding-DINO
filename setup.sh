803f4551efe4c7cb8e94d24a4d4cc8e8654e554d
Bindila-Bogdan <bogdan@meshh.com>
2025-06-16 00:15:50 +0200

    adjust setup for Grounding DINO and its open version

--- setup.sh
+++ setup.sh
@@ -1,10 +1,37 @@
-# create the environment
+#!/bin/bash
+source ~/.bashrc
+
+### SETUP FOR GROUNDING DINO ###
+# make sure the install is made at the system level
+conda deactivate
+
+# move into the GroundingDINO directory
+cd ../GroundingDINO/
+
+# set the CUDA_HOME variable and check it
+export CUDA_HOME=/usr/local/cuda
+echo $CUDA_HOME
+
+# remove the previously built GroundingDINO package
+rm -rf build dist *.egg-info
+
+# install pytorch
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+# build GroundingDINO
+pip install -e .
+
+### SETUP FOR OPEN GROUNDING DINO ###
+# move into the Open GroundingDINO directory
+cd ../Open-Grounding-DINO/
+
+# create a conda environment and activate it
 conda create --name open_grounding_dino python=3.12.3 --yes
 conda activate open_grounding_dino
 
-# install the dependencies
+# install pytorch and the dependencies
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
 # build deformable attention
 cd models/GroundingDINO/ops
